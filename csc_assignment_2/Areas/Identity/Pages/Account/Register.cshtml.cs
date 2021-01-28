6bdfaf5118c4c7e990e7d77962938c30e4b7dbe5
tohzenghau <tohzh.18@ichat.sp.edu.sg>
2021-01-28 14:44:07 +0800

    Update on the subscription

--- csc_assignment_2/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ csc_assignment_2/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -62,6 +62,9 @@ namespace csc_assignment_2.Areas.Identity.Pages.Account
             [DataType(DataType.Text)]
             [Display(Name = "Last Name")]
             public string LastName { get; set; }
+            [DataType(DataType.Text)]
+            [Display(Name = "Subscription Plan")]
+            public string SubPlan { get; set; }
         }
 
         public void OnGet(string returnUrl = null)
@@ -74,7 +77,7 @@ namespace csc_assignment_2.Areas.Identity.Pages.Account
             returnUrl = returnUrl ?? Url.Content("~/");
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email, FirstName = Input.FirstName, LastName = Input.LastName };
+                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email, FirstName = Input.FirstName, LastName = Input.LastName, SubPlan = Input.SubPlan };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
